sipXcom/sipxecs

commit e7b7c2c15a586a74feb93daa4ed56f12fde33be4

Apply patch for XECS-402 - users without voicemail privs can't record their name (for dial-by-name)
Patch from Huijun Yang.  Thanks!

--Woof!


git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11356 ab1d8caa-1f67-47f1-9e81-24633a41865c

sipXpbx/src/mailboxmgr/MailboxManager.cpp
@@ -1766,14 +1766,15 @@ MailboxManager::validateMailbox (
 
                     if ( permissions.getSize() > 0 )
                     {
-                        // Ensure user has 'Voicemail' permissions before proceeding
+                        // Ensure user has 'Voicemail'  or AutoAttendant permissions before proceeding
                         for ( int i=0; i<permissions.getSize(); i++ )
                         {
                             UtlHashMap record;
                             permissions.getIndex( i, record );
                             UtlString permission = *((UtlString*)record.findValue(&permissionKey));
 
-                            if ( permission.compareTo( "Voicemail", UtlString::ignoreCase ) == 0 )
+                            if ( permission.compareTo( "Voicemail", UtlString::ignoreCase ) == 0 ||
+                                 permission.compareTo( "AutoAttendant",  UtlString::ignoreCase ) == 0)
                             {
                                 // fixup to prevent infinite loop where the
                                 // uri does not have a permission set
@@ -1783,7 +1784,7 @@ MailboxManager::validateMailbox (
                                 // as we found a good set of permissions,
                                 validated = TRUE;
                                 OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                                              "MailboxManager::validateMailbox: found voicemail permission");
+                                              "MailboxManager::validateMailbox: found voicemail  or AutoAttendant permission");
                                 break;
                             }
                         }

sipXpbx/src/mailboxmgr/MailboxManager.h
@@ -112,6 +112,8 @@
 
 /** System administration permissions */
 #define RECORD_SYSTEM_PROMPTS_PERMISSION    "RecordSystemPrompts"
+#define ATTENDANTDIRECTORY_PERMISSION       "AutoAttendant"
+#define VOICEMAIL_PERMISSION                "Voicemail"
 
 
 /** Types of system prompts */

sipXcommserverLib/src/sipdb/SIPXAuthHelper.cpp
@@ -166,9 +166,10 @@ SIPXAuthHelper::isAuthorizedUser (
                         permissions.getIndex( i, record );
                         UtlString permission = *((UtlString*)record.findValue(&permissionKey));
 
-                        // AutoAttendant permission is never used here, it is only
-                        // used in creating the dialbyname database
-                        if ( permission.compareTo( "Voicemail", UtlString::ignoreCase )==0  )
+                        // AutoAttendant permission is used  here to allow user record name for dialbyname if its
+                        // Voicemail permission is not enabled.
+                        if ( permission.compareTo( "Voicemail", UtlString::ignoreCase )==0 || 
+                             permission.compareTo( "AutoAttendant", UtlString::ignoreCase )==0 )
                         {
                             permissionFound = TRUE;
 

sipXpbx/src/mailboxmgr/LoginCGI.cpp
@@ -159,6 +159,14 @@ LoginCGI::handleOpenVXIRequest( UtlString* out )
         if( bHasRecordSystemPromptsPermission )
             strRecordEnabled = "yes" ;
 
+        // Check if this user has permission to access voicemail options
+        UtlBoolean bHasVoicemailPermission = PermissionDB::getInstance()->
+                        hasPermission( mailboxUrl, VOICEMAIL_PERMISSION );
+        UtlString strVoicemailEnabled = "no" ;
+        if( bHasVoicemailPermission ) {
+            strVoicemailEnabled = "yes" ;
+        }
+
                 dynamicVxml += "<form>\n<block>\n";
                 dynamicVxml += "<var name=\"result\" expr=\"'success'\"/>\n";
 
@@ -170,7 +178,8 @@ LoginCGI::handleOpenVXIRequest( UtlString* out )
                                                         "<var name=\"saved\" expr=\"" + rSavedCount + "\"/>\n"\
                                                         "<var name=\"mailboxid\" expr=\"'" + m_mailboxIdentity + "'\"/>\n" \
                                                         "<var name=\"extension\" expr=\"'" + m_extension + "'\"/>\n" \
-                            "<var name=\"isRecordSystemPromptsEnabled\" expr=\"'" + strRecordEnabled + "'\"/>\n" ;
+                            "<var name=\"isRecordSystemPromptsEnabled\" expr=\"'" + strRecordEnabled + "'\"/>\n" \
+                            "<var name=\"isVoicemailEnabled\" expr=\"'" + strVoicemailEnabled + "'\"/>\n" ;
 
         } else
                 {
@@ -179,9 +188,10 @@ LoginCGI::handleOpenVXIRequest( UtlString* out )
                                                         "<var name=\"saved\" expr=\"0\"/>"\
                                                         "<var name=\"mailboxid\" expr=\"'" + m_mailboxIdentity + "'\"/>\n" \
                                                         "<var name=\"extension\" expr=\"'" + m_extension + "'\"/>\n" \
-                            "<var name=\"isRecordSystemPromptsEnabled\" expr=\"'" + strRecordEnabled + "'\"/>\n" ;
+                            "<var name=\"isRecordSystemPromptsEnabled\" expr=\"'" + strRecordEnabled + "'\"/>\n" \
+                            "<var name=\"isVoicemailEnabled\" expr=\"'" + strVoicemailEnabled + "'\"/>\n" ;
                 }
-                dynamicVxml +=  "<return namelist=\"result unheard total saved mailboxid extension isRecordSystemPromptsEnabled\"/>\n"\
+                dynamicVxml +=  "<return namelist=\"result unheard total saved mailboxid extension isRecordSystemPromptsEnabled isVoicemailEnabled\"/>\n"\
                                                 "</block>\n</form>\n";
         }
         else

sipXpbx/src/wwwcontent/vm_vxml/login.vxml.in
@@ -15,7 +15,8 @@
         <var name="from" />
         <var name="mediaserverurl" />
         <var name="securemediaserverurl" />
-    
+
+
         <!-- Action attribute is required by the CGI -->
         <var name="action" expr="'login'"/>
 
@@ -98,7 +99,14 @@
                 <if cond="login.result == 'success'">
                     <assign name="mailbox" expr="login.mailboxid" />
 		    <assign name="extn" expr="extension"/>
-                    <goto nextitem="playstatus"/>
+                    <assign name="isVoicemailEnabled" expr="login.isVoicemailEnabled" />
+                    <if cond="isVoicemailEnabled == 'yes'">
+                        <goto nextitem="playstatus"/>
+                    <else/>
+                        <!-- This routine is only entered when user's AttendantDirectory permission
+                        is enabled but Voicemail is not to allow user to record name for                                                          autoattendant dial-by-name  --> 
+                        <goto nextitem="recordname"/> 
+                    </if>
                 <elseif cond="login_attempts == '2'"/>
                     <!-- Extension and pin do not match. Only 3 attempts allowed. -->
                     <prompt bargein="false">
@@ -194,6 +202,20 @@
             <param name="securemediaserverurl" expr="securemediaserverurl" />
             <param name="isRecordSystemPromptsEnabled" expr="login.isRecordSystemPromptsEnabled" />
         </subdialog>
+
+         <!-- Proceed to the recordname menu -->
+         <subdialog name="recordname" cond="login.result == 'success'" method="get" src="recordgreeting.vxml">
+            <param name="mailbox" expr="mailbox" />
+            <param name="mediaserverurl" expr="mediaserverurl" />
+            <param name="securemediaserverurl" expr="securemediaserverurl" />
+            <param name="greetingtype" expr="'name'"/>
+            <filled>
+                <prompt bargein="false">
+                    <audio expr="mediaserverurl + promptsalias + 'thankyou_goodbye.wav'"/>
+                </prompt>
+                <disconnect/>
+            </filled>
+         </subdialog>
 
     </form>
 </vxml>

commit 0bad4002f07b7690e42bd1713c1709830e3d20fc

Fix for XECS-715: User without Voicemail permission but with     Autoattendent permission, is able to receive the voicemail
This problem was introduced in r11356 / XECS-402.  The logic in 
validateMailbox was updated to pass the permission checks if
either VoiceMail or the AutoAttendant permission was available.
This change was needed to allow the end-user to record their
name.

validateMailbox now looks at the requested/required permissions
instead of either.  The retrieve/login CGIs now require either
VoiceMail or AutoAttendant while all other CGIs now require
Voicemail (fixing this issue).



git-svn-id: http://sipxecs.sipfoundry.org/rep/sipXecs/main@11543 ab1d8caa-1f67-47f1-9e81-24633a41865c

sipXpbx/src/mailboxmgr/MailboxManager.cpp
@@ -54,14 +54,14 @@
 
 #ifdef USE_SOAP
 #define _SOAP_MSG_NO_ARG_TEMPLATE_ \
-	"<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"%s\" SOAP-ENV:encoding=\"%s\"" \
-	" xmlns:xsi=\"%s\"" \
-	" xmlns:xsd=\"%s\">" \
-	"<SOAP-ENV:Body>"\
-	"<m:%s xmlns:m=\"%s\">"\
-	"</m:%s>" \
-	"</SOAP-ENV:Body>"\
-	"</SOAP-ENV:Envelope>"
+    "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"%s\" SOAP-ENV:encoding=\"%s\"" \
+    " xmlns:xsi=\"%s\"" \
+    " xmlns:xsd=\"%s\">" \
+    "<SOAP-ENV:Body>"\
+    "<m:%s xmlns:m=\"%s\">"\
+    "</m:%s>" \
+    "</SOAP-ENV:Body>"\
+    "</SOAP-ENV:Envelope>"
 
 #define _SOAP_MSG_TEMPLATE_ \
    "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"%s\" SOAP-ENV:encoding=\"%s\"" \
@@ -202,11 +202,11 @@ MailboxManager::createMailbox ( const UtlString& mailboxIdentity )
     if( result == OS_SUCCESS )
     {
         if ( !OsFileSystem::exists( mailboxPath ) )
-	{
-          result = OsFileSystem::createDir( mailboxPath );
-          OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                      "MailboxManager::createMailbox: createDir('%s') = %d",
-                      mailboxPath.data(), result);
+        {
+            result = OsFileSystem::createDir( mailboxPath );
+            OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
+                    "MailboxManager::createMailbox: createDir('%s') = %d",
+                    mailboxPath.data(), result);
         }
 
         if ( result == OS_SUCCESS )
@@ -1303,7 +1303,7 @@ MailboxManager::saveMessage (
                      metaDataFile.close();
                      logContent = "Unable to write to message descriptor file : " + nameWithoutExtension + ".xml\n" ;
                      OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_ERR,
-				     "MailboxManager::saveMessage: metaDataFile.write() failed, filename '%s.xml'",
+                                   "MailboxManager::saveMessage: metaDataFile.write() failed, filename '%s.xml'",
                                    nameWithoutExtension.data());
                   }
                } else
@@ -1477,7 +1477,7 @@ MailboxManager::doLogin (
     OsStatus result = validateMailbox (
         loginString,       // userid/extension with/without domain
         TRUE,              // resolve extension
-        TRUE,              // check permissions
+        MB_REQUIRE_EITHER, // check permissions
         rMailboxIdentity,  // URL of actual mailbox found
         rExtension );
 
@@ -1666,7 +1666,7 @@ OsStatus
 MailboxManager::validateMailbox (
     const UtlString& loginString,
     const UtlBoolean& resolveExtensionFlag,
-    const UtlBoolean& checkPermissions,
+    const MailboxPermissions requiredPermissions,
     UtlString& rMailboxIdentity,
     UtlString& rExtension )
 {
@@ -1674,8 +1674,8 @@ MailboxManager::validateMailbox (
     UtlString logContent;
 
     OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                  "MailboxManager::validateMailbox: loginString = '%s', resolveExtensionFlag = %d, checkPermissions = %d",
-                  loginString.data(), resolveExtensionFlag, checkPermissions);
+                  "MailboxManager::validateMailbox: loginString = '%s', resolveExtensionFlag = %d, requiredPermissions = %d",
+                  loginString.data(), resolveExtensionFlag, requiredPermissions);
 
     // Hack debugging info
     logContent = "entering with arguments " + loginString;
@@ -1754,9 +1754,10 @@ MailboxManager::validateMailbox (
                              mailboxUrl.toString().data(), realmName.data(),
                              authType.data());
                 writeToLog( "validateMailbox", "found mailboxUrl", PRI_DEBUG );
+                
                 // Flag to indicate if we found correct permission for this credential
                 UtlBoolean permissionFound = FALSE;
-                if( checkPermissions )
+                if( requiredPermissions != MB_REQUIRE_NONE )
                 {
                     writeToLog( "validateMailbox", "checking permissions", PRI_DEBUG );
                     // check for 'Voicemail' permission entry in IMDB
@@ -1765,37 +1766,57 @@ MailboxManager::validateMailbox (
                         getPermissions( mailboxUrl, permissions );
 
                     if ( permissions.getSize() > 0 )
-                    {
-                        // Ensure user has 'Voicemail'  or AutoAttendant permissions before proceeding
+                    {                        
+                        // Ensure user has proper permissions before proceeding
                         for ( int i=0; i<permissions.getSize(); i++ )
                         {
                             UtlHashMap record;
                             permissions.getIndex( i, record );
                             UtlString permission = *((UtlString*)record.findValue(&permissionKey));
-
-                            if ( permission.compareTo( "Voicemail", UtlString::ignoreCase ) == 0 ||
-                                 permission.compareTo( "AutoAttendant",  UtlString::ignoreCase ) == 0)
+                            
+                            switch (requiredPermissions)
                             {
-                                // fixup to prevent infinite loop where the
-                                // uri does not have a permission set
-                                permissionFound = TRUE;
-
-                                // Exit the for loop with or without a good status
-                                // as we found a good set of permissions,
-                                validated = TRUE;
+                            case MB_REQUIRE_VOICEMAIL:
+                                if ( permission.compareTo( "Voicemail", UtlString::ignoreCase ) == 0)
+                                {
+                                    permissionFound = TRUE;                            
+                                }
+                                break ;
+                            case MB_REQUIRE_AUTOATTENDANT:
+                                if ( permission.compareTo( "AutoAttendant", UtlString::ignoreCase ) == 0)
+                                {
+                                    permissionFound = TRUE;
+                                }                                
+                                break ;
+                            case MB_REQUIRE_EITHER:
+                                if ( permission.compareTo( "Voicemail", UtlString::ignoreCase ) == 0 ||
+                                     permission.compareTo( "AutoAttendant",  UtlString::ignoreCase ) == 0 )
+                                {
+                                    permissionFound = TRUE;
+                                }                                
+                                break ;
+                            default:
+                                assert(false);
+                            }
+                            
+                            if (permissionFound)
+                            {
                                 OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                                              "MailboxManager::validateMailbox: found voicemail  or AutoAttendant permission");
-                                break;
+                                              "MailboxManager::validateMailbox: found requested permission (%d)", 
+                                              requiredPermissions);                                
+                                validated = TRUE;
+                                break ;                                
                             }
                         }
                     }
-                } else
+                } 
+                else
                 {
                     permissionFound = TRUE;
                     validated = TRUE;
                     OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
                                   "MailboxManager::validateMailbox: did not check permissions");
-            }
+                }
 
                 // it is possible that even though there is an uri defined
                 // for the user logging in, they may not have the persmission set
@@ -1821,7 +1842,7 @@ MailboxManager::validateMailbox (
                             m_mailstoreRoot + OsPathBase::separator +
                             MAILBOX_DIR + OsPathBase::separator +
                             mailboxId + OsPathBase::separator + 
-			    m_inboxFolder;
+                            m_inboxFolder;
                         OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
                                       "MailboxManager::validateMailbox: inboxDirName = '%s'",
                                       inboxDirName.data());
@@ -2678,7 +2699,7 @@ MailboxManager::updateMessageStates(
 
       if (messageids == REFRESH_ALL_MSG_STATES)
       {
-	 // Refresh message states, trigger NOTIFY
+         // Refresh message states, trigger NOTIFY
          msgCount = 1;         
       } 
       else if (messageids == UPDATE_ALL_MSG_STATES)
@@ -4474,8 +4495,8 @@ MailboxManager::generateDefaultGreetings (
             // Get the extension
             validateMailbox (
                 mailboxIdentity,
-                TRUE,           // RESOLVE EXTENSION
-                FALSE,          // CHECK PERMISSIONS
+                TRUE,             // RESOLVE EXTENSION
+                MB_REQUIRE_NONE,  // CHECK PERMISSIONS
                 identity,
                 extension );
 

sipXpbx/src/mailboxmgr/MailboxManager.h
@@ -178,6 +178,17 @@ public:
     UtlString mAfterHourMenu;
 };
 
+/**
+ * Mailbox Permissions used by validateMailbox
+ */
+typedef enum MailboxPermissions
+{
+    MB_REQUIRE_NONE          = 0, /**< Do not require any permissions */ 
+    MB_REQUIRE_VOICEMAIL     = 1, /**< Require voicemail only */    
+    MB_REQUIRE_AUTOATTENDANT = 2, /**< Require autoattendant only */
+    MB_REQUIRE_EITHER        = 3  /**< Require either voicemail or autoattendant */
+} MailboxPermissions ;
+
 
 /**
  * Mailbox Manager class.  This class is responsible for
@@ -298,7 +309,8 @@ public:
      * (it's actually a do while (!failed and !resolved ) loop).
      *
      * @param identityOrExtension   this is either an identity or an extension
-     * @param resolveAliasFlag      optionally resolve the best alias (preferably numeric)
+     * @param resolveExtensionFlag  optionally resolve the best alias (preferably numeric)
+     * @param requiredPermissions   verify required permissions
      * @param rMailboxIdentity      the resolved maibox identity (adfter searching the DBs)
      *                              this parameter is only returned if the resolveAliasFlag
      *                              is set to true
@@ -309,7 +321,7 @@ public:
     OsStatus validateMailbox (
         const UtlString& identityOrExtension,
         const UtlBoolean& resolveExtensionFlag,
-        const UtlBoolean& checkPermissions,
+        const MailboxPermissions requiredPermissions,
         UtlString& rMailboxIdentity,
         UtlString& rExtension ) ;
 

sipXpbx/src/mailboxmgr/ManageDistributionsWebCGI.cpp
@@ -590,7 +590,7 @@ ManageDistributionsWebCGI::validateAddress(const UtlString& address) const
    MailboxManager* pMailboxManager = MailboxManager::getInstance();
    OsStatus result = pMailboxManager->validateMailbox( address,
                                                        TRUE,
-                                                       TRUE,
+                                                       MB_REQUIRE_VOICEMAIL,
                                                        mailboxIdentity,
                                                        extension );
 

sipXpbx/src/mailboxmgr/PlayMessagesCGI.cpp
@@ -1346,12 +1346,11 @@ PlayMessagesCGI::validateFromUrl(const UtlString& fromStr,
    // construct the helper object for validating the from URL
    ValidateMailboxCGIHelper validateFromUrlHelper ( fromIdentity );
 
-   // check for voicemail permission only after
+   // NOTE: check for voicemail permission only after
    // confirming that the caller is an internal user
-   UtlBoolean checkPermission = FALSE;
 
    // check to see if the caller is an internal sipxchange user
-   OsStatus result = validateFromUrlHelper.validate( checkPermission );
+   OsStatus result = validateFromUrlHelper.validate( MB_REQUIRE_NONE );
    if( result == OS_SUCCESS )
    {
       // retrieve extension and mailbox identity
@@ -1363,9 +1362,8 @@ PlayMessagesCGI::validateFromUrl(const UtlString& fromStr,
       // Currently it checks the credential db to confirm that the identity
       // is valid before looking in the Permissions DB.
       ValidateMailboxCGIHelper checkPermissionHelper ( rFromMailboxIdentity );
-      checkPermission = TRUE;
       // check to see if the caller has voicemail inbox
-      OsStatus voicemailStatus = checkPermissionHelper.validate( checkPermission );
+      OsStatus voicemailStatus = checkPermissionHelper.validate( MB_REQUIRE_VOICEMAIL );
 
       if( voicemailStatus == OS_SUCCESS )
          rVoicemailEnabled = TRUE;

sipXpbx/src/mailboxmgr/RetrieveCGI.cpp
@@ -169,8 +169,11 @@ RetrieveCGI::handleOpenVXIRequest( UtlString* out )
 
 
    // Check if the call or login came from known mailbox user
+   // Allow retrieve even if the user has either voicemail or autoattendant
+   // permissions.  If the user only has AA, they are prompted to record
+   // their name.
    ValidateMailboxCGIHelper validateMailboxHelper( m_from );
-   OsStatus result = validateMailboxHelper.execute( out );
+   OsStatus result = validateMailboxHelper.validate(MB_REQUIRE_EITHER);
    UtlString fromIdentity;
    if( result == OS_SUCCESS )
       validateMailboxHelper.getMailboxIdentity( fromIdentity );

sipXpbx/src/mailboxmgr/TransferToExtnCGI.cpp
@@ -40,18 +40,17 @@ TransferToExtnCGI::execute(UtlString* out)
 {
     OsStatus result = OS_SUCCESS;
 
-        // Get fully qualified SIP URL for the extension
+    // Get fully qualified SIP URL for the extension
     Url extensionUrl;
-        MailboxManager* pMailboxManager = MailboxManager::getInstance();
-        pMailboxManager->getWellFormedURL( m_extension, extensionUrl );
+    MailboxManager* pMailboxManager = MailboxManager::getInstance();
+    pMailboxManager->getWellFormedURL( m_extension, extensionUrl );
 
-        // Validate the mailbox id and extension.
-        // If valid, lazy create the physical folders for the mailbox if necessary.
-    UtlBoolean checkPermissions = FALSE ;
+    // Validate the mailbox id and extension.
+    // If valid, lazy create the physical folders for the mailbox if necessary.
     ValidateMailboxCGIHelper validateMailboxHelper ( extensionUrl.toString() );
-    result = validateMailboxHelper.validate( checkPermissions );
+    result = validateMailboxHelper.validate( MB_REQUIRE_NONE );
     UtlString dynamicVxml;
-        if ( result == OS_SUCCESS )
+    if ( result == OS_SUCCESS )
     {
 /*
         UtlString transferUrlString;
@@ -73,20 +72,20 @@ TransferToExtnCGI::execute(UtlString* out)
             // Write out the dynamic VXML script to be processed by OpenVXI
     } else
     {
-            dynamicVxml =  getVXMLHeader() + 
-                        VXML_INVALID_EXTN_SNIPPET\
-                            VXML_END;
+        dynamicVxml =  getVXMLHeader() + 
+                       VXML_INVALID_EXTN_SNIPPET\
+                       VXML_END;
     }
 
-        if (out)
-        {
+    if (out)
+    {
         out->remove(0);
         UtlString responseHeaders;
         MailboxManager::getResponseHeaders(dynamicVxml.length(), responseHeaders);
 
         out->append(responseHeaders.data());
         out->append(dynamicVxml.data());
-        }
+    }
 
     return OS_SUCCESS;
 }

sipXpbx/src/mailboxmgr/ValidateMailboxCGIHelper.cpp
@@ -43,7 +43,7 @@ ValidateMailboxCGIHelper::~ValidateMailboxCGIHelper()
 OsStatus
 ValidateMailboxCGIHelper::execute( UtlString* out )
 {
-   OsStatus result = validate( TRUE ) ;
+   OsStatus result = validate() ;
    OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
                  "ValidateMailboxCGIHelper::execute: returns %d",
                  result);
@@ -51,7 +51,7 @@ ValidateMailboxCGIHelper::execute( UtlString* out )
 }
 
 OsStatus
-ValidateMailboxCGIHelper::validate( const UtlBoolean& checkPermissions )
+ValidateMailboxCGIHelper::validate( const MailboxPermissions requiredPermissions )
 {
     OsStatus result = OS_SUCCESS;
     // Check to see whether we have already validated the mailbox
@@ -61,10 +61,10 @@ ValidateMailboxCGIHelper::validate( const UtlBoolean& checkPermissions )
         result = validateIdentityAndGetExtension (
             m_mailboxIdentity,
             m_extension,
-            checkPermissions );
+            requiredPermissions );
         OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                      "ValidateMailboxCGIHelper::validate: checkPermissions = %d, returns m_mailboxIdentity = '%s', m_extension = '%s', result = %d",
-                      checkPermissions, m_mailboxIdentity.data(),
+                      "ValidateMailboxCGIHelper::validate: requiredPermissions = %d, returns m_mailboxIdentity = '%s', m_extension = '%s', result = %d",
+                      requiredPermissions, m_mailboxIdentity.data(),
                       m_extension.data(), result);
         m_isValidated = true;
     }
@@ -78,7 +78,7 @@ OsStatus
 ValidateMailboxCGIHelper::validateIdentityAndGetExtension (
     UtlString& rMailboxIdentity,
     UtlString& rExtension,
-    const UtlBoolean& checkPermissions)
+    const MailboxPermissions requiredPermissions)
 {
     OsStatus result = OS_SUCCESS;
     // look up the identity and extension from the IMDB
@@ -91,7 +91,7 @@ ValidateMailboxCGIHelper::validateIdentityAndGetExtension (
                     validateMailbox (
                         m_identityOrExtension,
                         TRUE,      // resolve extension
-                        checkPermissions,
+                        requiredPermissions,
                         m_mailboxIdentity,
                         m_extension);
         // pass internal results back
@@ -99,13 +99,13 @@ ValidateMailboxCGIHelper::validateIdentityAndGetExtension (
         rExtension = m_extension;
         m_isValidated = true;
         OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                      "ValidateMailboxCGIHelper::validateIdentityAndGetExtension: m_identityOrExtension = '%s', checkPermissions = %d, returns m_mailboxIdentity = '%s', m_extension = '%s', result = %d",
-                      m_identityOrExtension.data(), checkPermissions,
+                      "ValidateMailboxCGIHelper::validateIdentityAndGetExtension: m_identityOrExtension = '%s', requiredPermissions = %d, returns m_mailboxIdentity = '%s', m_extension = '%s', result = %d",
+                      m_identityOrExtension.data(), requiredPermissions,
                       m_mailboxIdentity.data(), m_extension.data(), result);
     }
     OsSysLog::add(FAC_MEDIASERVER_CGI, PRI_DEBUG,
-                  "ValidateMailboxCGIHelper::validateIdentityAndGetExtension: checkPermissions = %d, result = %d",
-                  checkPermissions, result);
+                  "ValidateMailboxCGIHelper::validateIdentityAndGetExtension: requiredPermissions = %d, result = %d",
+                  requiredPermissions, result);
     return result;
 }
 

sipXpbx/src/mailboxmgr/ValidateMailboxCGIHelper.h
@@ -14,6 +14,7 @@
 //#include <...>
 
 // APPLICATION INCLUDES
+#include "mailboxmgr/MailboxManager.h"
 #include "mailboxmgr/VXMLCGICommand.h"
 
 // DEFINES
@@ -48,9 +49,9 @@ public:
      * This does the work, lazy creates a mailbox if 
      * required and validates the identity/extension 
      */
-    virtual OsStatus execute (  UtlString* out = NULL);
+    virtual OsStatus execute (  UtlString* out = NULL );
 
-    OsStatus validate ( const UtlBoolean& checkPermissions = TRUE ) ;
+    OsStatus validate ( const MailboxPermissions requiredPermissions = MB_REQUIRE_VOICEMAIL ) ;
 
     /** Getter for the identity */
     void getMailboxIdentity( UtlString& mailboxIdentity ) const;
@@ -62,7 +63,7 @@ public:
     OsStatus validateIdentityAndGetExtension  ( 
         UtlString& rMailboxIdentity, 
         UtlString& rExtension,
-        const UtlBoolean& checkPermissions = TRUE );
+        const MailboxPermissions requiredPermissions = MB_REQUIRE_VOICEMAIL );
 
     UtlBoolean isNumeric( const UtlString& mailboxIdentity ) const ;
 