sipXcom/sipxecs

commit 08b986a11bc3fe6072c91d6cef14e9f93662cfe0

XX-9553 Mongo replication improvements - use entity id as Mongo internal _id - this is by default indexed - improves searches - eliminated the multiple searches that were done for a single entity (there were searches for each of the dataset generated - now there is only one per entity

sipXcommons/src/main/java/org/sipfoundry/commons/mongo/MongoConstants.java
@@ -14,7 +14,7 @@ public interface MongoConstants {
 
     static final String ALIASES = "als";
     static final String ALIAS = "alias";
-    static final String ID = "id";
+    static final String ID = "_id";
     static final String CONTACT = "cnt";
     static final String RELATION = "rln";
     static final String USERBUSYPROMPT = "bsyprmpt";

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/commserver.beans.xml
@@ -73,6 +73,7 @@
     <property name="coreContext" ref="coreContext" />
     <property name="forwardingContext" ref="forwardingContext" />
     <property name="externalAliases" ref="externalAliases" />
+    <property name="dataSetGenerator" ref="dataSetGenerator" />
   </bean>
 
   <bean id="sipxReplicationContextImpl"
@@ -150,7 +151,7 @@
     </property>
   </bean>
 
-  <bean id="dataSetGenerator" abstract="true"
+  <bean id="abstractDataSetGenerator" abstract="true"
     class="org.sipfoundry.sipxconfig.admin.commserver.imdb.DataSetGenerator">
     <property name="coreContext" ref="coreContext" />
   </bean>
@@ -170,6 +171,9 @@
     </property>
   </bean>
 
+  <bean id="dataSetGenerator" parent="abstractDataSetGenerator"
+    class="org.sipfoundry.sipxconfig.admin.commserver.imdb.DataSetGenerator">
+  </bean>
   <bean id="aliasDataSet" parent="dataSetGenerator"
     class="org.sipfoundry.sipxconfig.admin.commserver.imdb.Aliases">
   </bean>

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/AbstractDataSetGenerator.java
@@ -1,0 +1,70 @@
+/*
+ *
+ *
+ * Copyright (C) 2011 eZuce inc., certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the AGPL license.
+ *
+ * $
+ */
+package org.sipfoundry.sipxconfig.admin.commserver.imdb;
+
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
+import org.sipfoundry.sipxconfig.common.BeanWithId;
+import org.sipfoundry.sipxconfig.common.CoreContext;
+import org.sipfoundry.sipxconfig.common.Replicable;
+import org.sipfoundry.sipxconfig.common.SpecialUser;
+import org.sipfoundry.sipxconfig.common.User;
+
+public abstract class AbstractDataSetGenerator {
+    private DBCollection m_dbCollection;
+    private CoreContext m_coreContext;
+
+    public void setCoreContext(CoreContext coreContext) {
+        m_coreContext = coreContext;
+    }
+
+    protected CoreContext getCoreContext() {
+        return m_coreContext;
+    }
+
+    /**
+     * @return SIP domain - if not set uses m_coreContext to retrieve domain
+     */
+    protected String getSipDomain() {
+        return m_coreContext.getDomainName();
+    }
+
+    public DBCollection getDbCollection() {
+        return m_dbCollection;
+    }
+
+    public void setDbCollection(DBCollection dbCollection) {
+        m_dbCollection = dbCollection;
+    }
+
+    public abstract void generate(Replicable entity, DBObject top);
+
+    protected abstract DataSet getType();
+
+    public static String getEntityId(Replicable entity) {
+        String id = "";
+        if (entity instanceof BeanWithId) {
+            id = entity.getClass().getSimpleName() + ((BeanWithId) entity).getId();
+        }
+        if (entity instanceof SpecialUser) {
+            id = ((SpecialUser) entity).getUserName();
+        } else if (entity instanceof User) {
+            User u = (User) entity;
+            if (u.isNew()) {
+                id = u.getUserName();
+            }
+        } else if (entity instanceof ExternalAlias) {
+            ExternalAlias alias = (ExternalAlias) entity;
+            id = alias.getName();
+        }
+        return id;
+    }
+}

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/Aliases.java
@@ -24,9 +24,9 @@ public class Aliases extends DataSetGenerator {
         return DataSet.ALIAS;
     }
 
-    public void generate(Replicable entity) {
-        DBObject top = findOrCreate(entity);
+    public void generate(Replicable entity, DBObject top) {
         top.put(ALIASES, entity.getAliasMappings(getCoreContext().getDomainName()));
         getDbCollection().save(top);
     }
+
 }

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/Attendant.java
@@ -26,12 +26,11 @@ import static org.sipfoundry.commons.mongo.MongoConstants.*;
 public class Attendant extends DataSetGenerator {
 
     @Override
-    public void generate(Replicable entity) {
+    public void generate(Replicable entity, DBObject top) {
         if (!(entity instanceof User)) {
             return;
         }
         User user = (User) entity;
-        DBObject top = findOrCreate(user);
         // The following settings used to be in validusers.xml
         top.put(USERBUSYPROMPT, user.getSettingValue("voicemail/mailbox/user-busy-prompt")); // can
                                                                                              // be

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/CallerAliases.java
@@ -29,10 +29,9 @@ public class CallerAliases extends DataSetGenerator {
     }
 
     @Override
-    public void generate(Replicable entity) {
+    public void generate(Replicable entity, DBObject top) {
         if (entity instanceof User) {
             User user = (User) entity;
-            DBObject top = findOrCreate(user);
             if (StringUtils.isNotBlank(user.getSettingValue(UserCallerAliasInfo.EXTERNAL_NUMBER))) {
                 top.put(CALLERALIAS, SipUri.format(user.getDisplayName(),
                         user.getSettingValue(UserCallerAliasInfo.EXTERNAL_NUMBER), getSipDomain()));
@@ -43,7 +42,6 @@ public class CallerAliases extends DataSetGenerator {
         } else if (entity instanceof Gateway) {
             Gateway gateway = (Gateway) entity;
             final GatewayCallerAliasInfo gatewayInfo = gateway.getCallerAliasInfo();
-            DBObject top = findOrCreate(gateway);
             top.put("uid", Gateway.UID);
             if (StringUtils.isNotBlank(gatewayInfo.getDefaultCallerAlias())) {
                 top.put(CALLERALIAS, SipUri.fixWithDisplayName(gatewayInfo.getDefaultCallerAlias(),

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/Credentials.java
@@ -31,8 +31,7 @@ public class Credentials extends DataSetGenerator {
     }
 
     @Override
-    public void generate(Replicable entity) {
-        DBObject top = findOrCreate(entity);
+    public void generate(Replicable entity, DBObject top) {
         String realm = getCoreContext().getAuthorizationRealm();
         if (entity instanceof User) {
             User user = (User) entity;

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/DataSetGenerator.java
@@ -11,62 +11,29 @@ package org.sipfoundry.sipxconfig.admin.commserver.imdb;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
-import org.sipfoundry.sipxconfig.common.BeanWithId;
-import org.sipfoundry.sipxconfig.common.CoreContext;
 import org.sipfoundry.sipxconfig.common.Replicable;
-import org.sipfoundry.sipxconfig.common.SpecialUser;
-import org.sipfoundry.sipxconfig.common.User;
 
-import static org.sipfoundry.commons.mongo.MongoConstants.*;
+import static org.sipfoundry.commons.mongo.MongoConstants.ID;
+import static org.sipfoundry.commons.mongo.MongoConstants.IDENTITY;
+import static org.sipfoundry.commons.mongo.MongoConstants.VALID_USER;
 
-public abstract class DataSetGenerator {
-    private DBCollection m_dbCollection;
-    private CoreContext m_coreContext;
-
-    public void setCoreContext(CoreContext coreContext) {
-        m_coreContext = coreContext;
-    }
-
-    protected CoreContext getCoreContext() {
-        return m_coreContext;
-    }
-
-    /**
-     * @return SIP domain - if not set uses m_coreContext to retrieve domain
-     */
-    protected String getSipDomain() {
-        return m_coreContext.getDomainName();
-    }
-
-    public abstract void generate(Replicable entity);
-
-    protected abstract DataSet getType();
-
-    public DBCollection getDbCollection() {
-        return m_dbCollection;
-    }
-
-    public void setDbCollection(DBCollection dbCollection) {
-        m_dbCollection = dbCollection;
-    }
+public class DataSetGenerator extends AbstractDataSetGenerator {
 
     // We can safely assume that every replicable entity is a beanwithid
     // We can treat special cases separately
-    protected DBObject findOrCreate(Replicable entity) {
+    public DBObject findOrCreate(Replicable entity) {
         DBCollection collection = getDbCollection();
         String id = getEntityId(entity);
 
         DBObject search = new BasicDBObject();
         search.put(ID, id);
-        DBCursor cursor = collection.find(search);
-        DBObject top = new BasicDBObject();
-        if (!cursor.hasNext()) {
+        // DBObject cursor = collection.findOne(search);
+        DBObject top = collection.findOne(search);
+        if (top == null) {
+            top = new BasicDBObject();
             top.put(ID, id);
-        } else {
-            top = cursor.next();
         }
         if (entity.getIdentity(getSipDomain()) != null) {
             top.put(IDENTITY, entity.getIdentity(getSipDomain()));
@@ -80,22 +47,15 @@ public abstract class DataSetGenerator {
         return top;
     }
 
-    public static String getEntityId(Replicable entity) {
-        String id = "";
-        if (entity instanceof BeanWithId) {
-            id = entity.getClass().getSimpleName() + ((BeanWithId) entity).getId();
-        }
-        if (entity instanceof SpecialUser) {
-            id = ((SpecialUser) entity).getUserName();
-        } else if (entity instanceof User) {
-            User u = (User) entity;
-            if (u.isNew()) {
-                id = u.getUserName();
-            }
-        } else if (entity instanceof ExternalAlias) {
-            ExternalAlias alias = (ExternalAlias) entity;
-            id = alias.getName();
-        }
-        return id;
+    @Override
+    public void generate(Replicable entity, DBObject top) {
+        // empty implementation
     }
+
+    @Override
+    protected DataSet getType() {
+        // empty implementation
+        return null;
+    }
+
 }

commit 47fc340623c321876b187b22760139e906cdf6c1

- Bug fix: alias id field must be "id" and not "_id" the bug was introduced with the commit 08b986a11bc3fe6072c9 which changed the default mongo internal id - also this readds uid and ident for special users, which were removed with the same commit

sipXcommons/src/main/java/org/sipfoundry/commons/mongo/MongoConstants.java
@@ -15,6 +15,7 @@ public interface MongoConstants {
     static final String ALIASES = "als";
     static final String ALIAS = "alias";
     static final String ID = "_id";
+    static final String ALIAS_ID = "id";
     static final String CONTACT = "cnt";
     static final String RELATION = "rln";
     static final String USERBUSYPROMPT = "bsyprmpt";

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/admin/commserver/imdb/AliasMapping.java
@@ -9,8 +9,8 @@
  */
 package org.sipfoundry.sipxconfig.admin.commserver.imdb;
 
+import static org.sipfoundry.commons.mongo.MongoConstants.ALIAS_ID;
 import static org.sipfoundry.commons.mongo.MongoConstants.CONTACT;
-import static org.sipfoundry.commons.mongo.MongoConstants.ID;
 import static org.sipfoundry.commons.mongo.MongoConstants.RELATION;;
 
 /**
@@ -30,14 +30,14 @@ public class AliasMapping extends DataSetRecord {
      * @param contact
      */
     public AliasMapping(String identity, String contact, String relation) {
-        put(ID, identity);
+        put(ALIAS_ID, identity);
         put(CONTACT, contact);
         put(RELATION, relation);
     }
 
     // convenience methods
     public String getIdentity() {
-        return get(ID).toString();
+        return get(ALIAS_ID).toString();
     }
 
     public String getContact() {
@@ -48,7 +48,7 @@ public class AliasMapping extends DataSetRecord {
     }
 
     public void setIdentity(String ident) {
-        put(ID, ident);
+        put(ALIAS_ID, ident);
     }
 
     public void setContact(String cnt) {

sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/SpecialUser.java
@@ -11,6 +11,7 @@ package org.sipfoundry.sipxconfig.common;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -18,6 +19,8 @@ import java.util.Set;
 import org.apache.commons.lang.RandomStringUtils;
 import org.sipfoundry.sipxconfig.admin.commserver.imdb.AliasMapping;
 import org.sipfoundry.sipxconfig.admin.commserver.imdb.DataSet;
+
+import static org.sipfoundry.commons.mongo.MongoConstants.UID;
 
 public class SpecialUser extends BeanWithId implements Replicable {
 
@@ -102,8 +105,7 @@ public class SpecialUser extends BeanWithId implements Replicable {
 
     @Override
     public String getIdentity(String domain) {
-        // TODO Auto-generated method stub
-        return null;
+        return SipUri.stripSipPrefix(SipUri.format(null, getUserName(), domain));
     }
 
     @Override
@@ -113,7 +115,9 @@ public class SpecialUser extends BeanWithId implements Replicable {
 
     @Override
     public Map<String, Object> getMongoProperties(String domain) {
-        return Collections.EMPTY_MAP;
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put(UID, getUserName());
+        return props;
     }
 
 }

sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/acd/AcdLineTest.java
@@ -106,7 +106,7 @@ public class AcdLineTest extends BeanWithSettingsTestCase {
         m_line.setAcdServer(server);
 
         AliasMapping alias = m_line.getAliasMappings("mydomain.org").iterator().next();
-        assertEquals("555", alias.get(MongoConstants.ID));
+        assertEquals("555", alias.get("id"));
         assertEquals("myline@localhost:100", m_line.getIdentity("mydomain.org"));
 
         mcs.verify();

sipXconfig/neoconf/test/org/sipfoundry/sipxconfig/admin/callgroup/CallGroupContextImplTestDb.java
@@ -192,15 +192,15 @@ public class CallGroupContextImplTestDb extends SipxDatabaseTestCase {
 
         Iterator<AliasMapping> i = aliases.iterator();
         AliasMapping aliasMapping = i.next();
-        assertTrue(aliasMapping.get(MongoConstants.ID).toString().startsWith("sales"));
+        assertTrue(aliasMapping.get("id").toString().startsWith("sales"));
         assertTrue(aliasMapping.get(MongoConstants.CONTACT).toString().startsWith("<sip:default@pingtel.com>;q="));
 
         aliasMapping = i.next();
-        assertTrue(aliasMapping.get(MongoConstants.ID).toString().startsWith("401"));
+        assertTrue(aliasMapping.get("id").toString().startsWith("401"));
         assertTrue(aliasMapping.get(MongoConstants.CONTACT).toString().startsWith("sales"));
 
         aliasMapping = i.next();
-        assertTrue(aliasMapping.get(MongoConstants.ID).toString().startsWith("123456781"));
+        assertTrue(aliasMapping.get("id").toString().startsWith("123456781"));
         assertTrue(aliasMapping.get(MongoConstants.CONTACT).toString().startsWith("sales"));
     }
 