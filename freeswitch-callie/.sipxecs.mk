6e1981a243378ee5fbb65a321f3615f70b5f1042
dizzy <george@ezuce.com>
2015-06-04 23:38:05 +0200

    UC-3538: Config for mod_say is incomplete
    - build freeswitch callie RPMs

---
+++ freeswitch-callie/.sipxecs.mk
@@ -0,0 +1,9 @@
+freeswitch-callie_VER = 1.0.51
+freeswitch-callie_REL = 1
+freeswitch-callie_SRPM = freeswitch-sounds-en-us-callie-$(freeswitch-callie_VER)-$(freeswitch-callie_REL)$(RPM_DIST).src.rpm
+freeswitch-callie_SPEC = $(SRC)/$(PROJ)/freeswitch-sounds-en-us-callie.spec
+freeswitch-callie_SOURCES = \
+        freeswitch-sounds-en-us-callie-8000-1.0.51.tar.gz \
+	freeswitch-sounds-en-us-callie-16000-1.0.51.tar.gz \
+	freeswitch-sounds-en-us-callie-32000-1.0.51.tar.gz \
+	freeswitch-sounds-en-us-callie-48000-1.0.51.tar.gz
