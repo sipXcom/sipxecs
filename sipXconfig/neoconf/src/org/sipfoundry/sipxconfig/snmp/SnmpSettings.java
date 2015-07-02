1d361da6b49844ab1e424861e53a4760e271b868
Mircea Carasel <mirceac@ezuce.com>
2015-07-02 14:52:30 +0300

    UC-3680: Services no longer visible after changing SNMP community string
    -use saved community string when reading services data instead of hardcoded value (public)

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/snmp/SnmpSettings.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/snmp/SnmpSettings.java
@@ -26,6 +26,7 @@ import org.sipfoundry.sipxconfig.setting.SettingEntry;
 
 public class SnmpSettings extends PersistableSettings implements DeployConfigOnEdit {
     private static final String FIX_DEAD_PROCESSES = "cfdat/fix_dead_processes";
+    private static final String COMMUNITY_STRING = "cfdat/community_string";
     private boolean m_restartDefault;
 
     public SnmpSettings() {
@@ -63,5 +64,9 @@ public class SnmpSettings extends PersistableSettings implements DeployConfigOnE
 
     public boolean isFixDeadProcesses() {
         return (Boolean) getSettingTypedValue(FIX_DEAD_PROCESSES);
+    }
+
+    public String getCommunityString() {
+        return getSettingValue(COMMUNITY_STRING);
     }
 }
