d3d3f347e6efb5ea38218e7a014f3095235d4785
dizzy <george@ezuce.com>
2015-07-21 11:59:21 +0200

    UC-3577: 14.04 to 14.10 upgrade did not update call park orbit locations
    - update existing locations to primary location

--- sipXconfig/etc/sipxpbx/database/add-4.4-orbit-location.sql
+++ sipXconfig/etc/sipxpbx/database/add-4.4-orbit-location.sql
@@ -1,1 +1,2 @@
+update park_orbit set location_id=(select location_id from location where primary_location=true) WHERE location_id IS NULL;
 alter table park_orbit alter column location_id set default 1;
