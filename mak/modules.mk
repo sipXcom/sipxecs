68422a55ae7fb37b90ffdfd7d947678bc876c711
Andrei Vilcu <vilcu.andrei@gmail.com>
2015-07-29 16:58:47 +0300

    SIPX-81 Config support for call back on busy added build configuration

--- mak/modules.mk
+++ mak/modules.mk
@@ -55,7 +55,8 @@ sipx_extra = \
   sipXhomer \
   sipXcallQueue \
   sipXAocBilling \
-  sipXtools
+  sipXtools \
+  sipXcallback
 
 # sipxecs projects that are NOT essential for a running communication system
 # and are related to configuration system. Many are phone plugins
