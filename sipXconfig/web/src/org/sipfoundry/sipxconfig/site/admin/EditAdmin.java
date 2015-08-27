c97037aa35170ddf13d31d86e267d5e008425b57
Cristian Moisa <cmoisa@ezuce.com>
2015-08-27 14:34:24 +0300

    UC-2314 Add fields to confirm passwords on Admin Settings and SNMP pages
    Under Admin Settings added fields: Retype Password, Retype Voicemail Pin,
    Retype PostgreSQL Password
    Under SNMP added field: Retype community string
    
    Fixed some of the issues with precomit.

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/EditAdmin.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/admin/EditAdmin.java
@@ -76,6 +76,21 @@ public abstract class EditAdmin extends PageWithCallback implements PageBeginRen
         } catch (IllegalArgumentException ex) {
             throw new UserException(ex.getMessage());
         }
+        String defaultPassword = getSettings().getDefaultPassword();
+        String defaultPasswordConf = getSettings().getDefaultPasswordConfirmed();
+        if (!StringUtils.equals(defaultPassword, defaultPasswordConf)) {
+        	throw new UserException(getMessages().getMessage("error.unconfirmed.password"));
+        }
+        String defaultVmPin = getSettings().getDefaultVmPin();
+        String defaultVmPinConfirmed = getSettings().getVmpinDefaultConfirmed();
+        if (!StringUtils.equals(defaultVmPin, defaultVmPinConfirmed)) {
+        	throw new UserException(getMessages().getMessage("error.unconfirmed.pin"));
+        }
+        String defaultPostgresPassword = getSettings().getPostgresPassword();
+        String defaultPostgresPasswordConfirmed = getSettings().getPostgresPasswordConfirmed();
+        if (!StringUtils.equals(defaultPostgresPassword, defaultPostgresPasswordConfirmed)) {
+        	throw new UserException(getMessages().getMessage("error.unconfirmed.postgresql"));
+        }
         getAdminContext().saveSettings(getSettings());
     }
 }
