0ed8ca7bdca4c89e686c4cc8ac190aa879ddaeb0
dizzy <george@ezuce.com>
2015-09-17 16:52:49 +0300

    SIPX-50: When saving a group onSave() is triggered twice
    - refactor saveGroup from Core and Phone context

--- sipXconfig/web/src/org/sipfoundry/sipxconfig/site/branch/CallPermissionsPanel.java
+++ sipXconfig/web/src/org/sipfoundry/sipxconfig/site/branch/CallPermissionsPanel.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2015 sipXcom, certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ */
+
 package org.sipfoundry.sipxconfig.site.branch;
 
 import org.apache.tapestry.BaseComponent;
