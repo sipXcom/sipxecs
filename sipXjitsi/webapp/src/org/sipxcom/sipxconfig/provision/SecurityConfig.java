c640fb65bc7d7dd587de1c4fa3dedae9b29e68d2
Mircea Carasel <mirceac@ezuce.com>
2015-09-30 15:41:14 +0300

    SIPX-228: Provision Jitsi
    -re-use authenticationManager from sipXconfig

--- sipXjitsi/webapp/src/org/sipxcom/sipxconfig/provision/SecurityConfig.java
+++ sipXjitsi/webapp/src/org/sipxcom/sipxconfig/provision/SecurityConfig.java
@@ -5,12 +5,17 @@
  */
 package org.sipxcom.sipxconfig.provision;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @Configuration
 @EnableWebMvcSecurity
@@ -18,6 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MongoUserDetailsService userDetailsService;
+
+    @Autowired
+    private ServletContext context;
+
+    private static String SIPXCONFIG_PATH = "/sipxconfig";
+
+    private static String AUTHENTICATION_MANAGER = "authenticationManager";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        ServletContext sipxconfigCtx = context.getContext(SIPXCONFIG_PATH);
+        ApplicationContext webContext = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(sipxconfigCtx);
+        ProviderManager authManager = (ProviderManager) webContext.getBean(AUTHENTICATION_MANAGER);
+        auth.parentAuthenticationManager(authManager);
+        //auth.userDetailsService(userDetailsService);
     }
 }
