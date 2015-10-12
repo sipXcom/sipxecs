10366a8e45a3ba079424d66ae72ea1a2c3a02b19
Andrei Vilcu <vilcu.andrei@gmail.com>
2015-10-12 17:25:30 +0300

    SIPX-354 Elastic search and system audit check boxes checked by default on all nodes when updating to 15.10

--- sipXconfig/etc/sipxpbx/database/system_audit_remove.sql
+++ sipXconfig/etc/sipxpbx/database/system_audit_remove.sql
@@ -1,10 +1,10 @@
 insert into feature_local (feature_id, location_id) select 'systemaudit',l.location_id from
-location l where not exists(select 1 from setting_value where path='configserver-config/systemAudit');
+location l where not exists(select 1 from setting_value where path='configserver-config/systemAudit') and l.primary_location=true;
 insert into setup (setup_id) select 'systemaudit' from
-location l, setting_value s where l.name='Primary' and s.path='configserver-config/systemAudit';
+location l, setting_value s where l.primary_location='t' and s.path='configserver-config/systemAudit';
 
 insert into feature_local (feature_id, location_id) select 'elasticsearch',l.location_id from
-location l where not exists(select 1 from setting_value where path='configserver-config/systemAudit');
+location l where not exists(select 1 from setting_value where path='configserver-config/systemAudit') and l.primary_location=true;
 
 delete from setting_value where path='configserver-config/systemAudit';
 
