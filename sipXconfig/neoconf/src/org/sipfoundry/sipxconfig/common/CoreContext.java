6856368882bd8f5ff74c73c5953dfa3c8298de81
Andrei Vilcu <vilcu.andrei@gmail.com>
2015-12-22 15:24:57 +0200

    UC-3854 zen 5157: License count display problem when superadmin is phantom

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/CoreContext.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/common/CoreContext.java
@@ -227,4 +227,6 @@ public interface CoreContext extends DataObjectSource<User>, AliasOwner, Replica
     int getDisabledUsersCount();
 
     int getPhantomUsersCount();
+
+    int getPhantomUsersWithoutSuperadminCount();
 }
