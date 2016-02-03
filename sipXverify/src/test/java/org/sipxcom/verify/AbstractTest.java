c66cff347d38e386270795504c230a9b66c35ae0
Cristian Moisa <cristianmoisa@ymail.com>
2016-02-03 11:43:24 +0200

    UC-3847 Establish base of automation with Selenium - First phase

---
+++ sipXverify/src/test/java/org/sipxcom/verify/AbstractTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2016 sipXcom, certain elements licensed under a Contributor Agreement.
+ * Contributors retain copyright to elements licensed under a Contributor Agreement.
+ * Licensed to the User under the LGPL license.
+ */
+package org.sipxcom.verify;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.sipxcom.verify.util.LoginUtil;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import static org.testng.Assert.fail;
+import static org.testng.AssertJUnit.assertTrue;
+
+public abstract class AbstractTest {
+
+    protected WebDriver driver;
+
+    @BeforeSuite
+    //initialize WebDriver
+    public void init() {
+        driver = LoginUtil.getRemoteWebDriver(LoginUtil.SUPERADMIN);
+    }
+
+    @AfterSuite
+    public void cleanup() {
+        driver.close();
+    }
+
+    protected void clickOnItem(String xpath) {
+        WebElement element = driver.findElement(By.xpath(xpath));
+        element.click();
+    }
+
+    protected void clearField(String xpath){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        element.clear();
+    }
+
+    protected void sendKeysToField(String something, String xpath){
+        WebElement element = driver.findElement(By.xpath(xpath));
+        element.sendKeys(something);
+    }
+}
