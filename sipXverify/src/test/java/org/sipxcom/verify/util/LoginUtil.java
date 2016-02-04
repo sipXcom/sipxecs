7b499ae91b69efe2a5f4af9e36fbb9185d995e27
Cristian Moisa <cristianmoisa@ymail.com>
2016-02-04 17:32:33 +0200

    UC-3847 Added DB Connector,and more changes

--- sipXverify/src/test/java/org/sipxcom/verify/util/LoginUtil.java
+++ sipXverify/src/test/java/org/sipxcom/verify/util/LoginUtil.java
@@ -22,38 +22,11 @@ public class LoginUtil {
     private static final Log LOG = LogFactory.getLog(AbstractTest.class);
     public static final String SUPERADMIN = "superadmin";
 
-    private static Properties m_properties = new Properties();
-
-    static {
-        try {
-            InputStream propertiesFile = LoginUtil.class.getResourceAsStream("/conf.properties");
-            m_properties.load(propertiesFile);
-        } catch (Exception e) {
-            LOG.error("Error reading conf.properties:", e);
-            System.exit(1);
-        }
-    }
-
-    public static String getSiteURL() {
-        return m_properties.getProperty("site.url");
-    }
-
-    public static String getProperty(String propertyKey){
-        return m_properties.getProperty(propertyKey);
-    }
-
-    public static String getUserName(String userName) {
-        return m_properties.getProperty(userName + ".name");
-    }
-
-    public static String getUserPassword(String userName) {
-        return m_properties.getProperty(userName + ".password");
-    }
 
 
     private static void login(WebDriver driver, String userName) {
         driver.findElement(By.id("j_username")).sendKeys(userName);
-        driver.findElement(By.id("j_password")).sendKeys(getUserPassword(userName));
+        driver.findElement(By.id("j_password")).sendKeys(PropertyLoader.getUserPassword(userName));
         driver.findElement(By.id("login:submit")).click();
     }
 
@@ -61,7 +34,7 @@ public class LoginUtil {
      *  Returns selenium driver logged in with superadmin
      */
     public static WebDriver getRemoteWebDriver(String userName) {
-        String browser = m_properties.getProperty("browser");
+        String browser = PropertyLoader.getProperty("browser");
         WebDriver driver = null;
         if (browser.equals("chrome")) {
             return new ChromeDriver();
@@ -69,7 +42,7 @@ public class LoginUtil {
             driver = new FirefoxDriver();
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(getSiteURL());
+        driver.get(PropertyLoader.getSiteURL());
         login(driver, userName);
         return driver;
     }
