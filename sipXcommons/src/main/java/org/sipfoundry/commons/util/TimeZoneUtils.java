c8f13e8520bcb93b09e4d95dd1bc2c43fd7660f9
Andrei Vilcu <vilcu.andrei@gmail.com>
2016-02-10 14:40:25 +0200

    UC-3897 CDR Reports must be able to be reported by time zone

--- sipXcommons/src/main/java/org/sipfoundry/commons/util/TimeZoneUtils.java
+++ sipXcommons/src/main/java/org/sipfoundry/commons/util/TimeZoneUtils.java
@@ -2,7 +2,9 @@ package org.sipfoundry.commons.util;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDateTime;
@@ -23,4 +25,48 @@ public class TimeZoneUtils {
         calendar.add(Calendar.DAY_OF_MONTH, -daysAgo);
         return calendar.getTime();
     }
+
+    /**
+     * By default set start at next midnight
+     */
+    public static Date getDefaultEndTime(String timezone) {
+        Calendar now = Calendar.getInstance();
+        if (timezone != null) {
+            now.setTimeZone(TimeZone.getTimeZone(timezone));
+        }
+        now.add(Calendar.DAY_OF_MONTH, 1);
+        Calendar end = DateUtils.truncate(now, Calendar.DAY_OF_MONTH);
+        return end.getTime();
+    }
+
+    /**
+     *  start a day before end time
+     */
+    public static Date getDefaultStartTime(Date endTime, String timezone) {
+        Calendar then = Calendar.getInstance();
+        if (timezone != null) {
+            then.setTimeZone(TimeZone.getTimeZone(timezone));
+        }
+        then.setTime(endTime);
+        then.add(Calendar.DAY_OF_MONTH, -1);
+        return then.getTime();
+    }
+
+    /**
+     * Return a date with the same values as the input date but a different time zone
+     */
+    public static Date getSameDateWithNewTimezone(Date initialDate, TimeZone timezone) {
+        Calendar calTime = Calendar.getInstance();
+        calTime.setTime(initialDate);
+
+        Calendar cal = Calendar.getInstance(timezone);
+        cal.set(Calendar.YEAR, calTime.get(Calendar.YEAR));
+        cal.set(Calendar.MONTH, calTime.get(Calendar.MONTH));
+        cal.set(Calendar.DAY_OF_MONTH, calTime.get(Calendar.DAY_OF_MONTH));
+        cal.set(Calendar.HOUR_OF_DAY, calTime.get(Calendar.HOUR_OF_DAY));
+        cal.set(Calendar.MINUTE, calTime.get(Calendar.MINUTE));
+        cal.set(Calendar.SECOND, calTime.get(Calendar.SECOND));
+        return cal.getTime();
+    }
+
 }
