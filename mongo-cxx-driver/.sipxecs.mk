db68d76c97aad50c2b7e679313083fee027364b1
dizzy <george@ezuce.com>
2016-06-21 12:16:19 +0300

    UC-4104: mongo c++ driver fix for tcp connection issue

--- mongo-cxx-driver/.sipxecs.mk
+++ mongo-cxx-driver/.sipxecs.mk
@@ -1,11 +1,12 @@
 mongo-cxx-driver_VER = 2.6.7
-mongo-cxx-driver_REL = 3
+mongo-cxx-driver_REL = 4
 mongo-cxx-driver_SRPM = mongo-cxx-driver-$(mongo-cxx-driver_VER)-$(mongo-cxx-driver_REL)$(RPM_DIST).src.rpm
 mongo-cxx-driver_SPEC = $(SRC)/$(PROJ)/mongo-cxx-driver.spec
 mongo-cxx-driver_SOURCES = \
         mongo-cxx-driver-legacy-0.0-26compat-2.6.7.tar.gz \
 	$(SRC)/$(PROJ)/mongo-cxx-driver-2.6.7-logger.patch \
-	$(SRC)/$(PROJ)/mongo-cxx-driver-2.6.7-maxTimeMS.patch	
+	$(SRC)/$(PROJ)/mongo-cxx-driver-2.6.7-maxTimeMS.patch \
+	$(SRC)/$(PROJ)/mongo-cxx-driver-2.6.7-UC-4104.patch
 	                                                                                                                                     
 
 mongo-cxx-driver.dist:;
