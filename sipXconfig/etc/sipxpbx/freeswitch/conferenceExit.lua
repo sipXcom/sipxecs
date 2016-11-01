c64cbdd8809886766a164ac7a643d0cf69bcaba3
dizzy <george@ezuce.com>
2016-11-01 12:41:28 +0200

    Revert "IPX-522: Play name of person leaving conference"
    This reverts commit 1d47599a0faf9f0a9c7fb38ed620f9f101cb32ff.

--- sipXconfig/etc/sipxpbx/freeswitch/conferenceExit.lua
+++ sipXconfig/etc/sipxpbx/freeswitch/conferenceExit.lua
@@ -2,7 +2,7 @@ fileToPlay = argv[1]
 conference = argv[2]
 playPrompt = argv[3]
 api = freeswitch.API()
-if playPrompt == "true" then
+if playPrompt then
   api:executeString("conference "..conference.." play file_string://"..fileToPlay.."!conference/conf-has_left.wav")
 end
 os.remove(fileToPlay)
