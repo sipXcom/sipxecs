e0a6edb2b605c1ae6218f88646016bd57711796d
Cristian Moisa <cristianmoisa@ymail.com>
2016-11-07 12:45:36 +0200

    UC-3847 - Vibe Login as registered user, logout, login as guest user, different logout.

--- sipXverify/src/com/viewme/verify/LoginLogout.java
+++ sipXverify/src/com/viewme/verify/LoginLogout.java
@@ -20,16 +20,22 @@ public class LoginLogout extends AbstractTestViewMe {
     }
 
     @Test(dependsOnMethods = {"canLoginAsRegsteredUser"})
-    public void canLogout() throws FindFailed {
-        logout();
+    public void canLogoutFromMenu() throws FindFailed {
+        logoutFromMenu();
     }
 
-    @Test
+    @Test(dependsOnMethods = {"canLoginAsRegsteredUser","canLogoutFromMenu"})
     public void canLoginAsGuestUser() throws FindFailed {
         System.out.println("\n");
-        System.out.println("\n");
-        System.out.println("#### Running test " + this.getClass().getSimpleName()+" ####\n");
         openVibeApp();
+        loginAsGuestUser("guestEmailAddress","guestUsername","guestMeetingId");
+        System.out.println("\n");
+    }
+
+    @Test(dependsOnMethods = {"canLoginAsRegsteredUser","canLogoutFromMenu","canLoginAsGuestUser"})
+    public void canLogoutFromExitButton() throws FindFailed {
+        System.out.println("\n");
+        logoutFromExitButton();
         System.out.println("\n");
     }
 }
