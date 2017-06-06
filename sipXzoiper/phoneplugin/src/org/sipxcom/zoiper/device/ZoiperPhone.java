f83ea103766578f75492354adbdb369186d7ab28
Mircea Carasel <mirceac@ezuce.com>
2017-06-06 10:18:42 +0300

    UC-4439: Add config support for Zoiper soft phone
    -xmpp provisioning (not supported yet by zoiper)
    -provisioning fixes (STUN, Codecs, ZRTP algorithms not supported yet)

--- sipXzoiper/phoneplugin/src/org/sipxcom/zoiper/device/ZoiperPhone.java
+++ sipXzoiper/phoneplugin/src/org/sipxcom/zoiper/device/ZoiperPhone.java
@@ -5,17 +5,26 @@
  */
 package org.sipxcom.zoiper.device;
 
+import org.sipfoundry.sipxconfig.common.User;
 import org.sipfoundry.sipxconfig.device.Device;
+import org.sipfoundry.sipxconfig.device.DeviceDefaults;
 import org.sipfoundry.sipxconfig.device.Profile;
 import org.sipfoundry.sipxconfig.device.ProfileContext;
 import org.sipfoundry.sipxconfig.device.ProfileFilter;
 import org.sipfoundry.sipxconfig.phone.Line;
 import org.sipfoundry.sipxconfig.phone.LineInfo;
 import org.sipfoundry.sipxconfig.phone.Phone;
+import org.sipfoundry.sipxconfig.setting.SettingEntry;
 
 public class ZoiperPhone extends Phone {
 
     public static final String MIME_TYPE_PLAIN = "text/plain";
+    
+    private static final String IDENT = "generic/ident";
+    private static final String NAME = "generic/name";
+    private static final String USERNAME = "credentials/username";
+    private static final String PASSWORD = "credentials/password";
+    private static final String DOMAIN = "sip_credentials/SIP_domain";
     
     @Override
     protected void setLineInfo(Line line, LineInfo lineInfo) {
@@ -42,6 +51,11 @@ public class ZoiperPhone extends Phone {
 
         return profileTypes;
     }
+    
+    @Override
+    public void initializeLine(Line line) {
+        line.addDefaultBeanSettingHandler(new ZoiperLineDefaults(line));
+    }    
 
     static class PhoneProfile extends Profile {
         public PhoneProfile(String name) {
@@ -58,6 +72,49 @@ public class ZoiperPhone extends Phone {
             ZoiperPhone phone = (ZoiperPhone) device;
             return new ZoiperProfileContext(phone, phone.getModel().getProfileTemplate());
         }
-    }     
+    }
+    
+    public static class ZoiperLineDefaults {
+        private final Line m_line;
+        private final User m_user;
+
+        public ZoiperLineDefaults(Line line) {
+            m_line = line;
+            m_user = m_line.getUser();
+        }
+
+        @SettingEntry(path = IDENT)
+        public String getSipAccountUid() {
+            return m_line.getUserName() +
+                "@" + m_line.getPhoneContext().getPhoneDefaults().getDomainName();
+        }
+
+        @SettingEntry(path = PASSWORD)
+        public String getSipPassword() {
+            if (m_user == null) {
+                return null;
+            }
+            return m_user.getSipPassword();
+        }
+
+        @SettingEntry(path = DOMAIN)
+        public String getSipServerAddress() {
+            DeviceDefaults defaults = m_line.getPhoneContext().getPhoneDefaults();
+            return defaults.getDomainName();
+        }
+
+        @SettingEntry(path = USERNAME)
+        public String getUsername() {
+            return m_line.getUserName();
+        }
+
+        @SettingEntry(path = NAME)
+        public String getSipDisplayName() {
+            if (m_user == null) {
+                return null;
+            }
+            return m_user.getDisplayName();
+        }
+    }    
 
 }
