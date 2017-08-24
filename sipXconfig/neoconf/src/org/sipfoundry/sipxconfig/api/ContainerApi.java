e861291eb206bd2e7c7688b144a1f832e6a8aee3
mirceac <mircea.carasel@gmail.com>
2017-08-24 16:27:17 +0300

    SIPX-654: Manage proxy docker container from sipxconfig

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/api/ContainerApi.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/api/ContainerApi.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 @Description("Phone Management REST API")
 public interface ContainerApi {
 
+    @GET
     public ContainerBean getContainerBean(String containerName);
 
     @Path("{containerName}/json")
