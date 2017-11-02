ab6bfc48dc4db3474f7fa30dc0f85bdecc043abb
mirceac <mircea.carasel@gmail.com>
2017-11-02 14:14:41 +0200

    SIPX-697: Dockerize sipXprovision
    -accomodate sipxconfig

--- sipXprovision/config-plugin/src/org/sipfoundry/sipxconfig/provision/ProvisionConfiguration.java
+++ sipXprovision/config-plugin/src/org/sipfoundry/sipxconfig/provision/ProvisionConfiguration.java
@@ -29,9 +29,12 @@ import org.sipfoundry.sipxconfig.cfgmgt.ConfigRequest;
 import org.sipfoundry.sipxconfig.cfgmgt.ConfigUtils;
 import org.sipfoundry.sipxconfig.cfgmgt.KeyValueConfiguration;
 import org.sipfoundry.sipxconfig.commserver.Location;
+import org.springframework.beans.factory.annotation.Required;
 
 public class ProvisionConfiguration implements ConfigProvider {
     private Provision m_provision;
+    private String m_etcDir;
+    private String m_logDirectory;
 
     @Override
     public void replicate(ConfigManager manager, ConfigRequest request) throws IOException {
@@ -43,26 +46,47 @@ public class ProvisionConfiguration implements ConfigProvider {
         ProvisionSettings settings = m_provision.getSettings();
         for (Location location : locations) {
             boolean enabled = manager.getFeatureManager().isFeatureEnabled(Provision.FEATURE, location);
-            File dir = manager.getLocationDataDirectory(location);
+            File dir = getLocationDataDirectory(location);
             ConfigUtils.enableCfengineClass(dir, "sipxprovision.cfdat", enabled, "sipxprovision");
             if (!enabled) {
                 continue;
             }
-            Writer prov = new FileWriter(new File(dir, "sipxprovision-config.part"));
+            Writer prov = new FileWriter(new File(dir, "sipxprovision-config"));
             try {
-                write(prov, settings);
+                write(prov, manager, settings);
             } finally {
                 IOUtils.closeQuietly(prov);
             }
         }
     }
 
-    void write(Writer wtr, ProvisionSettings settings) throws IOException {
+    void write(Writer wtr, ConfigManager manager, ProvisionSettings settings) throws IOException {
         KeyValueConfiguration config = KeyValueConfiguration.equalsSeparated(wtr);
         config.writeSettings(settings.getSettings().getSetting("provision-config"));
+        config.write("log.file", m_logDirectory + "/sipxprovision.log");
+        config.write("provision.sipxchangeDomainName", manager.getDomainManager().getDomainName());
+        config.write("provision.hostname", "sipxprovision");
     }
 
     public void setProvision(Provision provision) {
         m_provision = provision;
+    }
+
+    private File getLocationDataDirectory(Location location) {
+        File d = new File(m_etcDir + "/conf", String.valueOf(location.getId()));
+        if (!d.exists()) {
+            d.mkdirs();
+        }
+        return d;
+    }
+
+    @Required
+    public void setEtcDir(String etcDir) {
+        m_etcDir = etcDir;
+    }
+
+    @Required
+    public void setLogDirectory(String logDir) {
+        m_logDirectory = logDir;
     }
 }
