25c04d592ecb362c5c24ec482f2beec205c86add
Mircea Carasel <mirceac@ezuce.com>
2020-01-09 15:58:41 +0200

    Improve registrar logging 2

--- sipXcommserverLib/src/sipdb/RegDB.cpp
+++ sipXcommserverLib/src/sipdb/RegDB.cpp
@@ -482,7 +482,8 @@ bool RegDB::getUnexpiredContactsUser(const string& identity, unsigned long timeN
         << " Expires: " << binding.getExpirationTime() - OsDateTime::getSecsSinceEpoch() << " sec"
         << " Call-Id: " << binding.getCallId()
         << " Time NOW: " << timeNow
-        << " Expiration TIME: " << binding.getExpirationTime());
+        << " Expiration TIME: " << binding.getExpirationTime()
+        << " Seconds Since Epoch: " << OsDateTime::getSecsSinceEpoch() << " SECONDS");
 
         push_or_replace_binding(bindings, binding);
       }
