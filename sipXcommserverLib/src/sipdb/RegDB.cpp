0f23829eff989de8057279ac553403273972801a
Mircea Carasel <mirceac@ezuce.com>
2020-02-19 12:45:13 +0200

    Revert "Improve registrar logging"
    This reverts commit e8d4580a21796abb5ebe2f03feba813e1fd8a32b.

--- sipXcommserverLib/src/sipdb/RegDB.cpp
+++ sipXcommserverLib/src/sipdb/RegDB.cpp
@@ -480,9 +480,7 @@ bool RegDB::getUnexpiredContactsUser(const string& identity, unsigned long timeN
         << " Identity: " << identity
         << " Contact: " << binding.getContact()
         << " Expires: " << binding.getExpirationTime() - OsDateTime::getSecsSinceEpoch() << " sec"
-        << " Call-Id: " << binding.getCallId()
-        << " Time NOW: " << timeNow
-        << " Expiration TIME: " << binding.getExpirationTime());
+        << " Call-Id: " << binding.getCallId());
 
         push_or_replace_binding(bindings, binding);
       }
